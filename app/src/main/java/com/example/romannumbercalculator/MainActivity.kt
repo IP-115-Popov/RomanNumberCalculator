554e910a15070b5f50374f710ed1671a66b5661c
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-12-11 15:20:40 +0700

    Added general OnClickListener

--- app/src/main/java/com/example/romannumbercalculator/MainActivity.kt
+++ app/src/main/java/com/example/romannumbercalculator/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.romannumbercalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.example.romannumbercalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val tvRezult: TextView = binding.tvRezult
+        val btnEqually:Button = binding.btnEqually
+
+        val bthOne: Button = binding.bthOne
+        val bthOneHundred:Button = binding.bthOneHundred
+        val bthPlus:Button = binding.bthPlus
+        val bthFive:Button = binding.bthFive
+        val bthFiveHundred:Button = binding.bthFiveHundred
+        val bthMinus:Button = binding.bthMinus
+        val bthTen:Button = binding.bthTen
+        val bthThousand:Button = binding.bthThousand
+        val bthMultiply:Button = binding.bthMultiply
+        val bthFifty:Button = binding.bthFifty
+        val bthErase:Button = binding.bthErase
+        val bthDivide:Button = binding.bthDivide
+
+        var equation: String = ""
+
+        val numericButton = setOf(
+            binding.bthOne, binding.bthOneHundred, binding.bthPlus, binding.bthFive, binding.bthFiveHundred,
+            binding.bthMinus,binding.bthTen,binding.bthThousand,binding.bthMultiply,binding.bthFifty, binding.bthErase,binding.bthDivide
+        )
+        for (button in numericButton)
+            button.setOnClickListener {
+                equation += button.text.toString()
+            }
+
     }
 }
