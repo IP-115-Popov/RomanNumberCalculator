5e39c891ef63e888805ddd4054c891fcf92b7508
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-12-11 16:51:27 +0700

    Invoice blank

--- app/src/main/java/com/example/romannumbercalculator/MainActivity.kt
+++ app/src/main/java/com/example/romannumbercalculator/MainActivity.kt
@@ -41,6 +41,7 @@ class MainActivity : AppCompatActivity() {
                 tvRezult.text = tvRezult.text.toString() + button.text.toString()
             }
         btnEqually.setOnClickListener {
+            //получение чисел и операторов
             var equation = tvRezult.text.toString()
             val numberList = equation.split(Regex("[+\\-*/]"))
 
@@ -49,11 +50,49 @@ class MainActivity : AppCompatActivity() {
 
             // Получение всех найденных operator в список
             val operatorList = matches.map { it.groupValues[1] }.toList()
+            //вычисление результата
+            val numberListInt = mutableListOf<Int>()
+            var rez = 0
+            numberList.forEach {
+                numberListInt.add(romanToInt(it))
+            }
+            operatorList.forEach {
 
-            tvRezult.text = "\n"+numberList.toString()+"\n"+operatorList.toString()
+            }
+
+
+            tvRezult.text = tvRezult.text.toString() + " = "
         }
         bthErase.setOnClickListener {
-            tvRezult.text.toString()
+            tvRezult.text = ""
         }
+    }
+    fun romanToInt(s: String): Int {
+        val romanMap = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+
+        var result = 0
+        var prevValue = 0
+
+        for (i in s.length - 1 downTo 0) {
+            val curValue = romanMap[s[i]]!!
+
+            if (curValue < prevValue) {
+                result -= curValue
+            } else {
+                result += curValue
+            }
+
+            prevValue = curValue
+        }
+
+        return result
     }
 }
